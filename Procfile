16223cefe258fa1ae6371f0b02fbf0a62182430d
dan560 <n8293881@qut.edu.au>
2019-10-18 17:08:39 +1000

    procfile change

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 2 'travel:create_app()'
+web: gunicorn -w 2 'marketplace:create_app()'
--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 import os
+import pytest
 
 from flask_login import LoginManager
 
