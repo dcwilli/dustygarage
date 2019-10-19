f690690e820c8a630ccea0b54ac89cdad64d3449
dan560 <n8293881@qut.edu.au>
2019-10-19 19:45:18 +1000

    set to zero

--- marketplace/tools.py
+++ marketplace/tools.py
@@ -85,7 +85,7 @@ def manage(id):
     print(sold_user)
 
     bid_user = ""
-    set_to_null = 0
+    set_to_zero = 0
     # If a user has not been marked as sold, show a list of current bids
     if sold_user == 0:
         heading = "Current Bids"
@@ -121,7 +121,7 @@ def manage(id):
         # redirect back to the manage page with refreshed list
         return redirect(url_for('tool.manage', id=id))
 
-    return render_template('tools/manage.html', soldForm=soldForm, userid=userid, tool=tool, heading=heading, undoForm=undoForm, bid_user=bid_user, set_to_null=set_to_null)
+    return render_template('tools/manage.html', soldForm=soldForm, userid=userid, tool=tool, heading=heading, undoForm=undoForm, bid_user=bid_user, set_to_zero=set_to_zero)
 
     # db_file_path = check_file(form)
 
