c5464352b6226ab7e241b37d229311a929872379
dan560 <n8293881@qut.edu.au>
2019-10-22 07:24:20 +1000

    commit

--- marketplace/userdash.py
+++ marketplace/userdash.py
@@ -40,26 +40,26 @@ def userselling(userid):
 
     print(userid)
     # query db for tools current user has listed
-    # tool = Tool.query.filter_by(user_id=userid).filter(
-    Tool.sold_status == 0).all()
-    tool_length=len(tool)
-    session['tool_length']=tool_length
+    tool = Tool.query.filter_by(user_id=userid).filter(
+        Tool.sold_status == 0).all()
+    tool_length = len(tool)
+    session['tool_length'] = tool_length
     print(tool)
     print("-----------Tool length below----------------------")
     print(tool_length)
-    return render_template('userdash/manageselling.html', userid = userid, tool = tool)
+    return render_template('userdash/manageselling.html', userid=userid, tool=tool)
 
 
-@bp.route('/userbids/<userid>', methods = ["POST", "GET"])
+@bp.route('/userbids/<userid>', methods=["POST", "GET"])
 @login_required
 def userbids(userid):
     # query db for bids current user has made
-    bids=db.session.query(Tool, Bid).join(
-        Bid).filter_by(user_id = userid).all()
+    bids = db.session.query(Tool, Bid).join(
+        Bid).filter_by(user_id=userid).all()
     print(bids)
-    current_user=session.get('user_id')
+    current_user = session.get('user_id')
 
     # if the url userid does not match the logged in user - log them out
     # if current_user != userid:
     #     return redirect(url_for('auth.logout'))
-    return render_template('userdash/managebids.html', userid = userid, bids = bids)
+    return render_template('userdash/managebids.html', userid=userid, bids=bids)
