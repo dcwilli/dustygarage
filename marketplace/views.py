0ed42386bd0626e771d8d929a18ecf1e4c133bbd
dan560 <n8293881@qut.edu.au>
2019-10-22 10:54:44 +1000

    commit

--- marketplace/views.py
+++ marketplace/views.py
@@ -22,7 +22,7 @@ bp = Blueprint("main", __name__)
 @bp.route("/", methods=["GET", "POST"])
 def index():
     tools = Tool.query.order_by(desc(Tool.date_created)).limit(4).all()
-
+    print(tools)
     form_land = LandingForm()
     print("Form has not validated")
     search_results = []
