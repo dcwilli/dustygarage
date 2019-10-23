1fd2e68df377a2d2ebd6922ff6f97888115f1a22
dan560 <n8293881@qut.edu.au>
2019-10-24 09:21:29 +1000

    c

--- marketplace/tools.py
+++ marketplace/tools.py
@@ -98,7 +98,9 @@ def manage(id):
 
         # join the user and bid table
         bid_user = db.session.query(User, Bid).filter(
-            Bid.user_id == User.id).filter(Bid.tool_id == Tool.id).filter(Tool.sold_status == Bid.user_id).all()
+            Bid.user_id == User.id)
+            
+            # .filter(Bid.tool_id == Tool.id).filter(Tool.sold_status == Bid.user_id).all()
 
     # User submits a mark as sold OR undo
     if request.method == "POST":
