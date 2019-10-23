4aa0fcf359408ee883aef47369e89829b8e12e0e
dan560 <n8293881@qut.edu.au>
2019-10-24 07:53:17 +1000

    c

--- marketplace/models.py
+++ marketplace/models.py
@@ -43,7 +43,7 @@ class Tool(db.Model):
     brand = db.Column(db.String(100))
     date_created = db.Column(db.DateTime, default=datetime.now())
     sold_status = db.Column(db.String(100), default=0)
-    sold_date = db.Column(db.String(50), default=0)
+    sold_date = db.Column(db.DateTime(50), default=datetime.now())
 
     bid_id = db.relationship('Bid', backref='tools')
 
