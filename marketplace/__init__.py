a7a68034b9eef3f29ad49dad4663c72d9c55c805
dan560 <n8293881@qut.edu.au>
2019-10-23 20:28:47 +1000

    z

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -61,14 +61,14 @@ def create_app():
     @app.errorhandler(404)
     def not_found(e):
         heading = "404"
-        image = "static/img/404.png"
+        image = "../static/img/404.png"
 
         return render_template("error.html", heading=heading, image=image), 404
 
     @app.errorhandler(500)
     def server_error(e):
         heading = "500, Internal Server Error"
-        image = "static/img/500.png"
+        image = "../static/img/500.png"
         return render_template("error.html", heading=heading, image=image), 500
 
     UPLOAD_FOLDER = "../static/img"
