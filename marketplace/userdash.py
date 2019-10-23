125ad9c41c3e67cae37f4572d218f62a8b377ab3
dan560 <n8293881@qut.edu.au>
2019-10-23 19:46:39 +1000

    a

--- marketplace/userdash.py
+++ marketplace/userdash.py
@@ -68,7 +68,8 @@ def userbids(userid):
     bids = db.session.query(Tool, Bid).join(
         Bid).filter_by(user_id=userid).all()
     print(bids)
-
+    if bids == None:
+        print("bids equal none")
     # get highest bid
     current_user = session.get('user_id')
 
