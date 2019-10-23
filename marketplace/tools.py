ea6c3ce15e8abe65760ebea4eda63720294fc819
dan560 <n8293881@qut.edu.au>
2019-10-24 09:36:36 +1000

    c

--- marketplace/tools.py
+++ marketplace/tools.py
@@ -98,7 +98,7 @@ def manage(id):
 
         # join the user and bid table
         bid_user = db.session.query(User, Bid).join(Bid).filter(
-            Bid.user_id == User.id).filter_by(tool_id=id).all()
+            Bid.user_id == User.id).filter_by(tool_id=id, user_id=sold_user).all()
 
         # .filter(Tool.sold_status == Bid.user_id).all()
         print(bid_user)
